63f76193960580108fb5aa3cfe4c79e2339be3b2
nandopeyton <68226901+nandopeyton@users.noreply.github.com>
2020-11-19 13:21:29 -0300

    fixed create new product

--- controllers/productController.js
+++ controllers/productController.js
@@ -37,20 +37,22 @@ module.exports = {
         }
 
         let product = await new Product(fields);
-        product.image = `https://carlitosbucket.s3-sa-east-1.amazonaws.com/${
-          product._id
-        }.${files.imageFile.type.replace("image/", "")}`;
-        await product.save();
-        let img = fs.readFileSync(files.imageFile.path);
-        let data = {
-          Bucket: "carlitosbucket",
-          Key: `${product._id}.${files.imageFile.type.replace("image/", "")}`,
-          ContentType: files.imageFile.type,
-          Body: img,
-        };
-        s3.putObject(data, async () => {
-          console.log("Successfully uploaded data to myBucket/myKey");
-        });
+        if (files.imageFile) {
+          product.image = `https://carlitosbucket.s3-sa-east-1.amazonaws.com/${
+            product._id
+          }.${files.imageFile.type.replace("image/", "")}`;
+          await product.save();
+          let img = fs.readFileSync(files.imageFile.path);
+          let data = {
+            Bucket: "carlitosbucket",
+            Key: `${product._id}.${files.imageFile.type.replace("image/", "")}`,
+            ContentType: files.imageFile.type,
+            Body: img,
+          };
+          s3.putObject(data, async () => {
+            console.log("Successfully uploaded data to myBucket/myKey");
+          });
+        }
 
         return res.json(product);
       });
